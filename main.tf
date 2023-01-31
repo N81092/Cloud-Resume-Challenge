52f965e7657a314b056693820c58d033df0463ef
N81092 <112365661+N81092@users.noreply.github.com>
2023-01-31 01:26:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,8 @@ provider "aws" {
 resource "aws_iam_role" "dynamodb_access" {
   #stub
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_assume_role" {
+  role       = aws_iam_role.dynamodb_access
+  policy_arn = aws_iam_policy.lambda_assume_role
+}
